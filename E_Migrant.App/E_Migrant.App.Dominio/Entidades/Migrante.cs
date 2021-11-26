c9cc8e36407636db02e76b4f8773a6ce6ac3c0c5
AndresFelipeCo <andresfelipe@udenar.edu.co>
2021-11-26 11:10:35 -0500

    Creando clases

---
+++ E_Migrant.App/E_Migrant.App.Dominio/Entidades/Migrante.cs
@@ -0,0 +1,19 @@
+using System;
+namespace E_Migrant.App.Dominio
+{
+    public class Migrante
+    {
+        public int Id {get; set;}
+        public string Nombre{get; set;}
+        public string Apellidos {get; set;}
+        public string TDocuento{get; set;}
+        public string Documento{get; set;}
+        public string PaisOrigen{get; set;}
+        public string FechaNacimiento{get; set;}
+        public string DirElectronica{get; set;}
+        public string Telefono{get; set;}
+        public string DirActual{get; set;}
+        public string Ciudad{get; set;}
+        public string SitLaboral{get; set;}        
+    }
+}
